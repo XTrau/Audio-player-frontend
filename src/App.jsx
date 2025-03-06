9225dd9f8accb18e64aad4f642fab92643a36bf2
Vildan Khismatullin <khismatullin.vildan@mail.ru>
2025-03-06 22:14:24 +0500

    update packages, fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Layout from './components/Layout/Layout'
 
-import { Routes, Route } from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 
 import Main from './pages/MainPage/MainPage'
 import AddArtistPage from './pages/Artist/AddArtistPage/AddArtistPage'
@@ -11,73 +11,73 @@ import './index.scss'
 import LoginPage from "./pages/Auth/LoginPage.jsx";
 import RegistrationPage from "./pages/Auth/RegistrationPage.jsx";
 import UserPage from "./pages/UserPage/UserPage.jsx";
-import { checkAuthenticated } from "./store/slices/authReducer.js";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import {checkAuthenticated} from "./store/slices/authReducer.js";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
 import AlbumPage from "./pages/Album/AlbumPage/AlbumPage.jsx";
 import MyMusicPage from "./pages/MyMusicPage/MyMusicPage.jsx";
-import { fetchLikedTrackIds } from "./store/slices/favouriteListReducer.js";
+import {fetchLikedTrackIds} from "./store/slices/favouriteListReducer.js";
 import About from "./pages/About/About.jsx";
 
 function App() {
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(checkAuthenticated());
-		dispatch(fetchLikedTrackIds());
-	}, [dispatch]);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(checkAuthenticated());
+        dispatch(fetchLikedTrackIds());
+    }, [dispatch]);
 
 
-	return (<>
-		<Layout>
-			<Routes>
-				<Route
-					path="/"
-					element={<Main/>}
-				/>
-				<Route
-					path="/login"
-					element={<LoginPage/>}
-				/>
-				<Route
-					path="/registration"
-					element={<RegistrationPage/>}
-				/>
-				<Route
-					path="/me"
-					element={<UserPage/>}
-				/>
-				<Route
-					path="/add_artist"
-					element={<AddArtistPage/>}
-				/>
-				<Route
-					path="/add_album"
-					element={<AddAlbumPage/>}
-				/>
-				<Route
-					path="/artist/:artist_id"
-					element={<ArtistPage/>}
-				/>
-				<Route
-					path="/album/:album_id"
-					element={<AlbumPage/>}
-				/>
-				<Route
-					path="/my_music"
-					element={<MyMusicPage/>}
-				/>
-				<Route
-					path="/create_playlist"
-					element={<MyMusicPage/>}
-				/>
-				<Route
-					path="/playlist/:playlist_id"
-					element={<MyMusicPage/>}
-				/>
-				<Route path="/about" element={<About/>}/>
-			</Routes>
-		</Layout>
-	</>)
+    return (<>
+        <Layout>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Main/>}
+                />
+                <Route
+                    path="/login"
+                    element={<LoginPage/>}
+                />
+                <Route
+                    path="/registration"
+                    element={<RegistrationPage/>}
+                />
+                <Route
+                    path="/me"
+                    element={<UserPage/>}
+                />
+                <Route
+                    path="/add_artist"
+                    element={<AddArtistPage/>}
+                />
+                <Route
+                    path="/add_album"
+                    element={<AddAlbumPage/>}
+                />
+                <Route
+                    path="/artist/:artist_id"
+                    element={<ArtistPage/>}
+                />
+                <Route
+                    path="/album/:album_id"
+                    element={<AlbumPage/>}
+                />
+                <Route
+                    path="/my_music"
+                    element={<MyMusicPage/>}
+                />
+                <Route
+                    path="/create_playlist"
+                    element={<MyMusicPage/>}
+                />
+                <Route
+                    path="/playlist/:playlist_id"
+                    element={<MyMusicPage/>}
+                />
+                <Route path="/about" element={<About/>}/>
+            </Routes>
+        </Layout>
+    </>)
 }
 
 export default App
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,36 +1,36 @@
 import "./About.scss"
 
-import React from 'react';
+function About() {
+    return (
+        <div>
+            <h2>О сайте</h2>
+            <div>
+                Сайт является учебным пет-проектом.
+            </div>
+            <div>
+                Разработчик: Студент ЮУрГУ Хисматуллин В.В. ЕТ-312 Прикладная математика и информатика 2024
+            </div>
 
-function About(props) {
-	return (
-		<div>
-			<h2>О сайте</h2>
-			<div>
-				Сайт является учебным пет-проектом.
-			</div>
-			<div>
-				Разработчик: Студент ЮУрГУ Хисматуллин В.В. ЕТ-312 Прикладная математика и информатика 2024
-			</div>
-
-			<h2>GitHub</h2>
-			<div>
-				<div className="git-hub-info">
-					<a href="https://github.com/XTrau" target="_blank">
-						<img width={75} height={75} src="../../images/GitHub.png" alt=""/>
-					</a>
-					<span>
+            <h2>GitHub</h2>
+            <div>
+                <div className="git-hub-info">
+                    <a href="https://github.com/XTrau" target="_blank">
+                        <img width={75} height={75} src="/GitHub.png" alt=""/>
+                    </a>
+                    <span>
 						<div>
-							<a href="https://github.com/XTrau/Audio-player-frontend" target="_blank">Frontend repository</a>
+							<a href="https://github.com/XTrau/Audio-player-frontend"
+                               target="_blank">Frontend repository</a>
 						</div>
 						<div>
-							<a href="https://github.com/XTrau/Audio-player-backend" target="_blank">Backend repository</a>
+							<a href="https://github.com/XTrau/Audio-player-backend"
+                               target="_blank">Backend repository</a>
 						</div>
 					</span>
-				</div>
-			</div>
-		</div>
-	);
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default About;
